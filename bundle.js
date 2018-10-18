220c9aabfa3302473d2d2561028093bde55033d6
Heesang Chae <theand@gmail.com>
2018-10-18 18:31:03 +0900

    add global namespace

--- bundle.js
+++ bundle.js
@@ -3,30 +3,7 @@
 
 const ics = require('ics')
 
-const event = {
-	  start: [2018, 5, 30, 6, 30],
-	  duration: { hours: 6, minutes: 30 },
-	  title: 'Bolder Boulder',
-	  description: 'Annual 10-kilometer run in Boulder, Colorado',
-	  location: 'Folsom Field, University of Colorado (finish line)',
-	  url: 'http://www.bolderboulder.com/',
-	  geo: { lat: 40.0095, lon: 105.2669 },
-	  categories: ['10k races', 'Memorial Day Weekend', 'Boulder CO'],
-	  status: 'CONFIRMED',
-	  organizer: { name: 'Admin', email: 'Race@BolderBOULDER.com' },
-	  attendees: [
-		      { name: 'Adam Gibbons', email: 'adam@example.com', rsvp: true },
-		      { name: 'Brittany Seaton', email: 'brittany@example2.org' }
-		    ]
-}
-
-ics.createEvent(event, (error, value) => {
-	  if (error) {
-		      console.log(error)
-		    }
-
-	  console.log(value)
-})
+window.ics = ics;
 
 
 },{"ics":24}],2:[function(require,module,exports){
