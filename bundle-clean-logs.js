f65a8ec4117e4dd26057031a1a97bbd3e8879bee
Heesang Chae <theand@gmail.com>
2018-10-30 18:48:52 +0900

    don't overwrite console.

--- bundle-clean-logs.js
+++ bundle-clean-logs.js
@@ -4,8 +4,7 @@
 const logging = require('clean_logs');
 const logger = logging.logger;
 
-logger.info = logger.debug;
-window.console = logger;
+window.logger = logger;
 
 
 
--- main-clean-logs.js
+++ main-clean-logs.js
@@ -3,7 +3,6 @@
 const logging = require('clean_logs');
 const logger = logging.logger;
 
-logger.info = logger.debug;
-window.console = logger;
+window.logger = logger;
 
 
